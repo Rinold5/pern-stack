f15587c32ce7b9a08f910dfb0c184730ecc517c0
Rinold5 <rinoldacquin2614@gmail.com>
2025-03-17 12:37:56 +0530

    Theme

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,8 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("preferred-theme") || "forest",
+  theme: localStorage.getItem("preferred-theme") || "luxury",
   setTheme: (theme) => {
+    console.log("Setting theme to:", theme); // Debugging
     localStorage.setItem("preferred-theme", theme);
     set({ theme });
   },
